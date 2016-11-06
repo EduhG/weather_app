45ecfe9f966b5472a59089a147de2d9744e7f5b9
eduhg <edwinnyangena@gmail.com>
2016-11-06 11:13:17 +0300

    refactored home_page view to use templates

--- temp_viewer/views.py
+++ temp_viewer/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_page(request):
-    pass
+    template_view = 'index.html'
+
+    return render(request, template_view)
